8977ef19959624deed6fd03659c842be85e6b6a0
kapilkalra04 <kapilkalra04@gmail.com>
2018-07-12 16:46:47 +0530

    Made detection.py modular

--- src/detection.py
+++ src/detection.py
@@ -5,58 +5,81 @@ import cv2
 def convertToRGB(img): 
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# specify locations of the 	model and its weights
-args = {}
-args["prototxt"] = "src/deploy.prototxt.txt"					# model-definition
-args["model"] = "src/res10_300x300_ssd_iter_140000.caffemodel"	# pre-trained weights
-args["image"] = "data/images/test4.jpeg"							# images are loaded as 3D matrix - (h x w x c)
-args["confidence"] = 0.75										# confidence>value is a face
+def detect(model,weights,image,confidence):
 
-# load the caffe model 
-print "[INFO] Loading model"
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
- 
-# load the input image 
-image = cv2.imread(args["image"])
+	# specify locations of the 	model and its weights
+	args = {}
+	args["model"] = model					# model-definition
+	args["weights"] = weights						# pre-trained weights
+	args["image"] = image						# images are loaded as 3D matrix - (h x w x c)
+	args["confidence"] = confidence				# when confidence>value then it is a face
 
-# print len(image)							# height of the image
-# print len(image[0])						# width of the image
-# print len(image[0][0])					# no of color-channels 
-# print image.shape							# stores h,w,c values
-(h, w) = image.shape[:2]
+	# load the caffe model 
+	print "[INFO] Loading model"
+	# net = cnn used to detect faces
+	net = cv2.dnn.readNetFromCaffe(args["model"], args["weights"])
+	 
+	# load the input image 
+	image = cv2.imread(args["image"])
 
-# construct an input blob for the image
-# by resizing to a fixed 300x300 pixels and then normalizing it 
-# along with doing a mean subtraction
-blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)),
- 1.0, (300, 300), (104.0, 177.0, 123.0))
+	# print len(image)							# height of the image
+	# print len(image[0])						# width of the image
+	# print len(image[0][0])					# no of color-channels 
+	# print image.shape							# stores h,w,c values
+	(h, w) = image.shape[:2]
 
-print "[INFO] Computing face detections..."
-net.setInput(blob)
-detections = net.forward()
+	# construct an input blob for the image
+	# by resizing to a fixed 300x300 pixels and then normalizing it 
+	# along with doing a mean subtraction
+	blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)),
+	 1.0, (300, 300), (104.0, 177.0, 123.0))
 
-count = 0
-for i in range(0, detections.shape[2]):
-	# extract the confidence (i.e., probability) associated with the
-	# prediction
-	confidence = detections[0, 0, i, 2]
+	print "[INFO] Computing face detections..."
+	net.setInput(blob)
+	detections = net.forward()
+
+	count = 0									# count of no of faces detected
+	faces = {}									# stores the faces rectangles co-ordinates
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the
+		# prediction
+		confidence = detections[0, 0, i, 2]
+		
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for the
+			# face
+			faces[i] = []
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")	# extracting integral values
+			# adding area details along with co-ordinate values
+			faces[i].extend([startX,endX,startY,endY,((endX-startX)*(endY-startY))])
+
+			# plotting the face rectangle	
+			x = []
+			y = []
+
+			# plot the box
+			x.extend([startX,endX,endX,startX,startX])
+			y.extend([startY,startY,endY,endY,startY])
+			plt.plot(x,y)
+			count = count + 1
+
+	print "Faces Detected = " + str(count)
+	print faces
+
+
+	largestFaceIndex = -1
+	largestAreaYet = 0
 	
-	# filter out weak detections by ensuring the `confidence` is
-	# greater than the minimum confidence
-	if confidence > args["confidence"]:
-		# compute the (x, y)-coordinates of the bounding box for the
-		# object
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")	# extracting integral values
-		x = []
-		y = []
+	for i in range(0,len(faces)):
+		if(faces[i][4]>largestAreaYet):
+			largestFaceIndex = i
+			largestAreaYet = faces[i][4]
 
-		# plot the box
-		x.extend([startX,endX,endX,startX,startX])
-		y.extend([startY,startY,endY,endY,startY])
-		plt.plot(x,y)
-		count = count + 1
+	print largestAreaYet
+	print largestFaceIndex
 
-print "Faces Detected = " + str(count)
-plt.imshow(convertToRGB(image))
-plt.show() 
+	plt.imshow(convertToRGB(image))
+	plt.show() 
