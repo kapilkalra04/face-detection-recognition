a6aaab2af74e34322bd4f95fde8e9dd62c0ef9a8
kapilkalra04 <kapilkalra04@gmail.com>
2018-08-10 20:06:58 +0530

    Added comments to the code-base to improve readability

--- src/detection.py
+++ src/detection.py
@@ -83,3 +83,18 @@ def detect(model,weights,image,isPath):
 			largestAreaYet = faces[i][4]
 
 	return convertToRGB(image),convertToGRAY(image),faces[largestFaceIndex]
+
+
+if __name__ == '__main__':
+	model = "src/deploy.prototxt.txt"							# model-definition
+	weights = "src/res10_300x300_ssd_iter_140000.caffemodel"	# pre-trained weights
+	image = "data/images/demo1.JPG"								# image name reqd. images are loaded as 3D matrix - (h x w x c)	
+	isPath = True
+	print "Hello"
+	plt.subplot(2,1,1)
+	colorImage, grayImage, mainFaceBox = detect(model,weights,image,isPath)
+	plt.imshow(colorImage)
+	plt.subplot(2,1,2)
+	mainFaceGray = grayImage[mainFaceBox[2]:mainFaceBox[3], mainFaceBox[0]:mainFaceBox[1]]
+	plt.imshow(mainFaceGray)
+	plt.show()
--- src/pre_processing2.py
+++ src/pre_processing2.py
@@ -8,44 +8,46 @@ def detectMainFace(imageName,isPath):
 	weights = "src/res10_300x300_ssd_iter_140000.caffemodel"	# pre-trained weights
 	image = imageName											# image name reqd. images are loaded as 3D matrix - (h x w x c)	
 
+	# send for face detection
 	colorImage, grayImage, mainFaceBox = detection.detect(model,weights,image,isPath)
 	
+	# crop the misaligned face from the whole image
 	mainFaceGray = grayImage[mainFaceBox[2]:mainFaceBox[3], mainFaceBox[0]:mainFaceBox[1] ]
 	
 	return colorImage, mainFaceGray, mainFaceBox
 
 def alignImage(colorImage,mainFaceGray,mainFaceBox):
+	# obtain eye centers
 	left_eye_center_x,left_eye_center_y,right_eye_center_x,right_eye_center_y = alignment.detectEyeCenters(mainFaceGray)
 	
+	# obtain affine transformation values
 	center, angle, scale = alignment.rotate(mainFaceGray,left_eye_center_x,left_eye_center_y,right_eye_center_x,right_eye_center_y)
 	
-	X = [left_eye_center_x,right_eye_center_x]
-	Y = [left_eye_center_y,right_eye_center_y]
-	# plt.plot(X,Y,'-D',markersize=3)
-
-
-	# update co-ordinates according to colorImage
+	# update co-ordinates according to colorImage the orignal iage
 	left_eye_center_x = left_eye_center_x + mainFaceBox[0]
 	right_eye_center_x = right_eye_center_x + mainFaceBox[0]
 	left_eye_center_y = left_eye_center_y + mainFaceBox[2]
 	right_eye_center_y = right_eye_center_y + mainFaceBox[2]
-	
 	center = (center[0]+mainFaceBox[0],center[1]+mainFaceBox[2])
 
+	# perform affine transformation
 	M = cv2.getRotationMatrix2D(center, angle, scale)
-	
 	alignedImage = cv2.warpAffine(colorImage,M,(colorImage.shape[1],colorImage.shape[0]),flags=cv2.INTER_CUBIC)
 	
 	return alignedImage, left_eye_center_x,left_eye_center_y,right_eye_center_x,right_eye_center_y
 
 
 def getFace(imagePath):
+	# detect the misaligned largest face in gray
 	colorImage, mainFaceGray, mainFaceBox = detectMainFace(imagePath,True)
 	
+	# straighten the actual image
 	alignedImage, e1x, e1y, e2x, e2y = alignImage(colorImage,mainFaceGray,mainFaceBox)
 	
+	# detect the aligned largest face in gray
 	colorImage, mainFaceGray, mainFaceBox = detectMainFace(alignedImage,False)
 	
+	# apply denoising
 	mainFaceGray = cv2.fastNlMeansDenoising(mainFaceGray)									# denoising
 	
 	return mainFaceGray																		# returns a grayscaled,aligned,(256,256) face
@@ -53,7 +55,7 @@ def getFace(imagePath):
 if __name__ == '__main__':
 	
 	plt.subplot(2,2,1)
-	colorImage, mainFaceGray, mainFaceBox = detectMainFace('data/images/test9.JPG',True)
+	colorImage, mainFaceGray, mainFaceBox = detectMainFace('data/library/train/17.JPG',True)
 	plt.imshow(colorImage)
 
 	plt.subplot(2,2,2)
